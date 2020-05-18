88acac865c59f0a3343382bec552e942309706c4
maxwilliams361 <52715538+maxwilliams361@users.noreply.github.com>
2020-05-18 14:43:54 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,27 @@
+$(".buttonHide").click(function(){
+    $("img").hide();
+});
+$(".buttonShow").click(function(){
+    $("img").show();
+});
+$(".buttonToggle").click(function(){
+    $("img").toggle();
+});
+$(".buttonSlideUp").click(function(){
+    $("img").slideUp();
+});
+$(".buttonSlideDown").click(function(){
+    $("img").slideDown();
+});
+$(".buttonSlideToggle").click(function(){
+    $("img").slideToggle();
+});
+$(".buttonFadeIn").click(function(){
+    $("img").fadeIn();
+});
+$(".buttonFadeOut").click(function(){
+    $("img").fadeOut();
+});
+$(".buttonFadeToggle").click(function(){
+    $("img").fadeToggle();
+});
